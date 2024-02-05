f8bc0e2015d6482a3492d25e7625e6cb7587bddf
Sriganth V <sriganth3@live.com>
2024-02-05 23:54:57 +0530

    [Design Patterns] Added Singleton Design Pattern

---
+++ src/designpatterns/singleton/Singleton.java
@@ -0,0 +1,30 @@
+package designpatterns.singleton;
+
+public class Singleton {
+
+	private static volatile Singleton instance;
+	
+	public static long value;
+
+	private Singleton() {
+
+	}
+
+	public static Singleton getInstance() {
+		if(instance == null) {
+			synchronized(Singleton.class) {
+				if(instance == null) {
+					System.out.print("Singleton instance is created");
+					instance = new Singleton();
+					value = System.currentTimeMillis();
+
+				}else {
+					System.out.print("returning Existing Singleton instance");
+				}
+			}
+		}else {
+			System.out.print("returning Existing Singleton instance");
+		}
+		return instance;
+	}
+}
